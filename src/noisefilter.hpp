6b8f0cb2a79d1dd4f34f8b150ec67649bfd5cb6f
Marat Fayzullin <luarvique@gmail.com>
2025-06-21 23:20:51 -0400

    Optimizing NoiseFilter.

--- src/noisefilter.hpp
+++ src/noisefilter.hpp
@@ -3,10 +3,12 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <csdr/complex.hpp>
+#include <csdr/noisefilter.hpp>
 
 #include "module.hpp"
 
 struct NoiseFilter: Module {
+    Csdr::AFNoiseFilter *noiseFilter;
     int threshold = 0;
     uint32_t wndSize = 32;
     uint32_t fftSize = 4096;

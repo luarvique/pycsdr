f30cfddcb16eeea9c9539ca6babddbc280f1c8a7
Marat Fayzullin <luarvique@gmail.com>
2025-07-23 22:55:07 -0400

    Adding support for more formats to resampler and limiter.

--- src/audioresampler.cpp
+++ src/audioresampler.cpp
@@ -3,17 +3,31 @@
 #include <csdr/audioresampler.hpp>
 
 static int AudioResampler_init(AudioResampler* self, PyObject* args, PyObject* kwds) {
-    static char* kwlist[] = {(char*) "inputRate", (char*) "outputRate", NULL};
+    static char* kwlist[] = {(char*) "inputRate", (char*) "outputRate", (char*) "format", NULL};
 
     unsigned int inputRate = 0;
     unsigned int outputRate = 0;
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "II", kwlist, &inputRate, &outputRate)) {
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "II|O!", kwlist, &inputRate, &outputRate, FORMAT_TYPE, &self->inputFormat)) {
         return -1;
     }
 
-    self->inputFormat = FORMAT_FLOAT;
-    self->outputFormat = FORMAT_FLOAT;
-    self->setModule(new Csdr::AudioResampler(inputRate, outputRate));
+    self->outputFormat = self->inputFormat;
+
+    if (self->inputFormat == FORMAT_FLOAT) {
+        self->setModule(new Csdr::AudioResampler<float>(inputRate, outputRate));
+    } else if (self->inputFormat == FORMAT_SHORT) {
+        self->setModule(new Csdr::AudioResampler<short>(inputRate, outputRate));
+    } else if (self->inputFormat == FORMAT_COMPLEX_FLOAT) {
+        self->setModule(new Csdr::AudioResampler<complex<float>>(inputRate, outputRate));
+    } else if (self->inputFormat == FORMAT_COMPLEX_SHORT) {
+        self->setModule(new Csdr::AudioResampler<complex<short>>(inputRate, outputRate));
+    } else {
+        PyErr_SetString(PyExc_ValueError, "unsupported format");
+        return -1;
+    }
+
+    Py_INCREF(self->inputFormat);
+    Py_INCREF(self->outputFormat);
 
     return 0;
 }
--- src/limit.cpp
+++ src/limit.cpp
@@ -5,16 +5,30 @@
 #include <csdr/window.hpp>
 
 static int Limit_init(Limit* self, PyObject* args, PyObject* kwds) {
-    static char* kwlist[] = {(char*) "maxAmplitude", NULL};
+    static char* kwlist[] = {(char*) "format", (char*) "maxAmplitude", NULL};
 
     float maxAmplitude = 1.0f;
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|f", kwlist, &maxAmplitude)) {
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|O!f", kwlist, FORMAT_TYPE, &self->inputFormat, &maxAmplitude)) {
         return -1;
     }
 
-    self->inputFormat = FORMAT_FLOAT;
-    self->outputFormat = FORMAT_FLOAT;
-    self->setModule(new Csdr::Limit(maxAmplitude));
+    self->outputFormat = self->inputFormat;
+
+    if (self->inputFormat == FORMAT_FLOAT) {
+        self->setModule(new Csdr::Limit<float>(maxAmplitude));
+    } else if (self->inputFormat == FORMAT_SHORT) {
+        self->setModule(new Csdr::Limit<short>(maxAmplitude));
+    } else if (self->inputFormat == FORMAT_COMPLEX_FLOAT) {
+        self->setModule(new Csdr::Limit<complex<float>>(maxAmplitude));
+    } else if (self->inputFormat == FORMAT_COMPLEX_SHORT) {
+        self->setModule(new Csdr::Limit<complex<short>>(maxAmplitude));
+    } else {
+        PyErr_SetString(PyExc_ValueError, "unsupported format");
+        return -1;
+    }
+
+    Py_INCREF(self->inputFormat);
+    Py_INCREF(self->outputFormat);
 
     return 0;
 }

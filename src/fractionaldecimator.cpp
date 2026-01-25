bd2b596b36842782fb5d8c6ae1c6127609578e65
Marat Fayzullin <luarvique@gmail.com>
2026-01-24 23:30:18 -0500

    Adding stereo FM support by Shadowman94.

--- src/fractionaldecimator.cpp
+++ src/fractionaldecimator.cpp
@@ -2,6 +2,7 @@
 #include "types.hpp"
 
 #include <csdr/fractionaldecimator.hpp>
+#include <csdr/fmstereo.hpp>
 #include <csdr/window.hpp>
 
 template <typename T>
@@ -54,4 +55,60 @@ PyType_Spec FractionalDecimatorSpec = {
     0,
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_FINALIZE,
     FractionalDecimatorSlots
+};
+
+template <typename T>
+static void setupStereoDecimator(StereoFractionalDecimator* self, float rateMPX, float decimation, unsigned int numPolyPoints, bool prefilter) {
+    Csdr::LowPassFilter<T>* filter = nullptr;
+    if (prefilter) {
+        auto w = new Csdr::HammingWindow();
+        float transition = 0.06f;
+        filter = new Csdr::LowPassFilter<T>(0.5 / (decimation - transition), transition, w);
+    }
+    self->setModule(new Csdr::StereoFractionalDecimator<T>(rateMPX, decimation, numPolyPoints, filter));
+}
+
+static int StereoFractionalDecimator_init(StereoFractionalDecimator* self, PyObject* args, PyObject* kwds) {
+    static char* kwlist[] = {
+        (char*) "format",
+        (char*) "rateMPX",
+        (char*) "decimation",
+        (char*) "numPolyPoints",
+        (char*) "prefilter", NULL};
+
+    PyObject* format;
+    float rateMPX = 0.0f;
+    float decimation = 0.0f;
+    unsigned int numPolyPoints = 12;
+    int prefilter = false;
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!ff|Ip", kwlist, FORMAT_TYPE, &format, &rateMPX, &decimation, &numPolyPoints, &prefilter)) {
+        return -1;
+    }
+
+    if (format == FORMAT_FLOAT) {
+        setupStereoDecimator<float>(self, rateMPX, decimation, numPolyPoints, prefilter);
+    } else {
+        PyErr_SetString(PyExc_ValueError, "unsupported stereo fractional decimator format");
+        return -1;
+    }
+
+    Py_INCREF(format);
+    self->inputFormat = format;
+    Py_INCREF(format);
+    self->outputFormat = format;
+
+    return 0;
+}
+
+static PyType_Slot StereoFractionalDecimatorSlots[] = {
+    {Py_tp_init, (void*) StereoFractionalDecimator_init},
+    {0, 0}
+};
+
+PyType_Spec StereoFractionalDecimatorSpec = {
+    "pycsdr.modules.StereoFractionalDecimator",
+    sizeof(StereoFractionalDecimator),
+    0,
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_FINALIZE,
+    StereoFractionalDecimatorSlots
 };
--- src/fractionaldecimator.hpp
+++ src/fractionaldecimator.hpp
@@ -6,5 +6,7 @@
 #include "module.hpp"
 
 struct FractionalDecimator: Module {};
+struct StereoFractionalDecimator: Module {};
 
 extern PyType_Spec FractionalDecimatorSpec;
+extern PyType_Spec StereoFractionalDecimatorSpec;
